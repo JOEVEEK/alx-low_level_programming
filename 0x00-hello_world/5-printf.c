4b8df74b0b182fb280309cfc03a893c4aabc66fc
Victor Joshua <victorjoshua52@gmail.com>
2022-09-08 22:09:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing
+ *
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	puts("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
