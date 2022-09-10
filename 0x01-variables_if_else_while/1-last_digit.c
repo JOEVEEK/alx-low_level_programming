3c11041b8ee574637f9cacf153121ab96e8e692d
Victor Joshua <victorjoshua52@gmail.com>
2022-09-09 19:47:30 -0700

    task 1 correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 
 if (n > 5)
 {
-	printf("Last dugit of %d is %d and is greater than 5\n", n, 1);
+	printf("Last digit of %d is %d and is greater than 5\n", n, 1);
 }
 else if (1 == 0)
 {
-	printf("Last dugit of %d is %d and is 0\n", n, 1);
+	printf("Last digit of %d is %d and is 0\n", n, 1);
 }
 else
 {
-	printf("Last dugit of %d is %d is less than 6 and is not 0\n", n, 1);
+	printf("Last digit of %d is %d is less than 6 and is not 0\n", n, 1);
 }
 return (0);
 }
