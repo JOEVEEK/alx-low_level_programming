bb1492df81ac50dbfa5f73f5e71e69cb983730c1
Victor Joshua <victorjoshua52@gmail.com>
2022-09-09 21:08:28 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 	char low;
-
 	for (low = 'a'; low <= 'z'; low++)
-
-		putchar(low);
-	for (low = 'A'; low <= 'z'; low++)
-
+{
 	putchar(low);
-	printf("\n");
-
+}
+	for (low = 'A'; low <= 'z'; low++)
+{
+	putchar(low);
+}
+	putchar("\n");
 return (0);
 }
