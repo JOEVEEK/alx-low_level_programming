8eb5d682c9c271c04b67ef5b72467c3189ef0a61
Victor Joshua <victorjoshua52@gmail.com>
2022-09-11 20:30:44 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int c;
 
-	for (c = '32'; c <= '41'; c++)
+	for (c = "32"; c <= "41"; c++)
 	{
 		putchar(c);
-		if (c != '41')
+		if (c != "41")
 		{
 			putchar(',');
 			putchar(' ');
