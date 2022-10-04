f34479782f27ec647275923eda08b03e8c52d4a4
Victor Joshua <victorjoshua52@gmail.com>
2022-10-04 03:52:24 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the
+ * string given as a parameter.
+ * @str: string to be copied
+ *
+ * Return: NULL if error occurs, pointer to allocated space
+ */
+
+char *_strdup(char *str)
+{
+	char *cpy;
+	int index, lens;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		index++;
+	cpy = malloc(sizeof(char) * (lens + 1));
+
+	if (cpy == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
+	}
+
+	cpy[len] = '\0';
+
+	return (cpy);
+}
