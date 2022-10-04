d0ad7ae80f3daf3799030a076e21cd25988661c2
Victor Joshua <victorjoshua52@gmail.com>
2022-10-04 12:44:48 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,61 +1,51 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * len - returns length of str
- * @str: string counted
+ * argstostr - function that concatenates all the arguments of your program.
+ * @ac: input
+ * @av: input
  *
- * Return: returns the length
- */
-
-int len(char *str)
-{
-	int len = 0;
-
-	if (str != NULL)
-	{
-		while (str[len])
-			len++;
-	}
-	return (len);
-}
-
-/**
- * arstostr - a function that concatenates all the arguments of your program
- * @ac: count of ars passed to the function
- * @av: array of arguments
- *
- * Return: pointer to the new string
+ * Return: a pointer to a new string, or NULL if it fails
+ * NULL if ac == 0 or av == NULL
  */
 
 char *argstostr(int ac, char **av)
 {
-	char *new_string = NULL;
-	int k = 0, i = ac, j, sum = 0, temp = 0;
-
+	int i;
+	int j;
+	int l;
+	char *s;
+	
 	if (ac == 0 || av == NULL)
-		return (NULL);
-	while (ac--)
-		sum += (len(av[ac] + 1);
-	new_string = (char) malloc(sum + 1);
-
-	if (new_string != NULL)
 	{
-		while (k < i)
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
 		{
-			for (j = 0; av[k][j] != '\0'; j++)
-				new_string[j + temp] = av[k][j];
-			new_string[temp + j] = '\n';
-			temp += (j + 1);
-			k++;
+			j++;
 		}
-		new_string[temp] = '\0';
+		l += j + 1;
 	}
-	else
+	s = malloc(sizeof(char) * (l + 1));
+	if (s == NULL)
 	{
 		return (NULL);
 	}
-	return (new_string);
+	l = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			*(s + l) = av[i][j];
+			l++;
+		}
+		*(s + l) = '\n';
+		l++;
+	}
+	return (s);
 }
-
-
-
