7552e072465bde82f83a7c4754c1a1be401c1f58
Victor Joshua <victorjoshua52@gmail.com>
2022-10-06 22:54:45 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,9 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b - memory to allocate
+ * malloc_checked - allocates a memory using malloc
+ * @b: memory to allocate
  *
  * Return: pointer to allocated memory or normal process termination if error
  */
