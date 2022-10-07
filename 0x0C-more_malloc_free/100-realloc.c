5fbbc7c0098410d65834d39d81903ddecd9d9662
Victor Joshua <victorjoshua52@gmail.com>
2022-10-06 23:48:14 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,8 +34,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	if (new_size > old_size)
 		max = old_size;
+
 	for (i = 0; i < max; i++)
 		p[i] = oldp[i];
 	free(ptr);
+
 	return (p);
 }
