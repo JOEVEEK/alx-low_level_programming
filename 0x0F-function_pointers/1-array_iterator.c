e27d6b190d72bd9b70b69a86b4d0a4be1442cbd7
Victor Joshua <victorjoshua52@gmail.com>
2022-10-12 14:20:32 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,25 +4,16 @@
  * int_index - searches for an integer
  * @array: array to serch through
  * @size: size of array
- * @cmp: function used to compare
- *
- * Return: first index for which cmp doesn't return 0, or -1
+ * @action: pointer to function used
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, r;
+	unsigned int i;
 
-	if (size > 0 && array && cmp)
-	{
-		for (i = 0; i < size; i++)
-		{
-			r = cmp(array[i]);
-			if (r)
-				break;
-		}
-		if (i < size)
-			return (i);
-	}
-	return (-1);
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
