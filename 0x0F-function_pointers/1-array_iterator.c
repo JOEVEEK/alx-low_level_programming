2ecd53ae535197a288531f3c7b511108ae81748e
Victor Joshua <victorjoshua52@gmail.com>
2022-10-12 14:36:12 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array.
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array.
  * @array: array to serch through
  * @size: size of array
  * @action: pointer to function used
