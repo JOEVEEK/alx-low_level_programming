388a8cd51aeb7555d9e0d6e25f9182dc8ae2abbc
Victor Joshua <victorjoshua52@gmail.com>
2022-10-12 14:30:24 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,7 @@
  * @action: pointer to function used
  */
 
-int int_index(int *array, int size, int (*cmp)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
